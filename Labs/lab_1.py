6d49f8e167c7dd2ad70c0a224a6d9336107a9b52
Luis Cortes <lacortes@cpp.edu>
2017-07-27 12:09:59 -0700

    Completed lab 1

--- Labs/lab_1.py
+++ Labs/lab_1.py
@@ -1,5 +1,10 @@
 #!/usr/local/bin/python3
 
+# Luis Cortes
+# CS 299
+# Lab 1
+# 07/27/17
+
 def main():
     # Prompt user to enter name
     name = input("Please enter your name: ")
@@ -13,11 +18,21 @@ def main():
 
     # Prompt user to enter salary 
     salary = input("Please enter salary: ")
-    salary = float(salary)
+    salary = int(salary)
 
     # Display the information back to the user
-    print("My name is {0}, my age is {1}\n\
-        I hope to work for {2}, and earn ${3} per year".format(name, ))
+    print("My name is {0}, my age is {1}\nI hope to work for {2}, and earn ${3} per year"
+        .format(name, age, company_name, salary))
 
 if __name__ == "__main__" : 
     main()
+
+# OUTPUT    
+'''
+Please enter your name: Luis Cortes
+Please enter your age: 23
+Please enter company name: Target
+Please enter salary: 120000
+My name is Luis Cortes, my age is 23
+I hope to work for Target, and earn $120000 per year
+'''
