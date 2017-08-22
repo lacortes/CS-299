3cb982e6b67ed1fb648355be4f369589719b2a31
Luis Cortes <lacortes@cpp.edu>
2017-08-22 10:14:13 -0700

    Project 3 added

---
+++ Projects/Project_3/project_3_luis_cortes.py
@@ -0,0 +1,28 @@
+# /usr/local/bin/python3
+
+
+def get_size():
+    pizza_sizes = [10, 12, 14, 16]
+    tries = 3
+
+    while tries > 0:
+        print("##########################")
+        print("#         Sizes          #")
+        print("#************************#")
+        print("#       10 inches        #")
+        print("#       12 inches        #")
+        print("#       14 inches        #")
+        print("#       16 inches        #")
+        print("##########################")
+        print()
+        user_size = int(input("Enter pizza size: "))
+
+
+def main():
+    print("+========================+")
+    print("+       CPP Pizza        +")
+    print("+========================+")
+
+
+if __name__ == "__main__":
+    main()
